126454ea692c34db9718ceb0c24a36a304d32533
faith rono <faithrono132@gmail.com>
2024-06-13 22:15:19 +0000

    updates

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Script to create the users table with a country enumeration
+
+-- Create the users table if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
