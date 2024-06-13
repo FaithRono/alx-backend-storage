9793327bbc9d8cc2360f79ea86148fc63f7ecb33
faith rono <faithrono132@gmail.com>
2024-06-13 23:39:23 +0000

    updates

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,11 @@
--- 2-fans.sql
--- Calculate the number of fans for each country origin of bands
--- and rank them by the number of fans in descending order.
+-- Task 2: Best band ever! - ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, COUNT(*) as nb_fans
-FROM bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- Select distinct origin and sum of fans for each origin
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans`
+    -- From the metal_bands table
+    FROM `metal_bands`
+    -- Group the results by origin
+    GROUP BY `origin`
+    -- Order the results by the total number of fans in descending order
+    ORDER BY `nb_fans` DESC;
+
