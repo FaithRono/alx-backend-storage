4119da98e77715307aae8bfc3d301f036c9142bc
faith rono <faithrono132@gmail.com>
2024-06-13 22:35:06 +0000

    updates

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- 4-store.sql
+-- Create trigger to decrease quantity after adding a new order
+DELIMITER //
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
